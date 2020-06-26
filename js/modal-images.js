85399bca886f54ce293e9877989b3ec581328c6a
KrisdiFire <65904004+KrisdiFire@users.noreply.github.com>
2020-06-26 18:52:53 +0200

    Add files via upload

---
+++ js/modal-images.js
@@ -0,0 +1,20 @@
+document.getElementById("myModal").style.display = "none";
+var modal = document.getElementById("myModal");
+var i;
+
+var img = document.getElementsByClassName("image_galery");
+var modalImg = document.getElementById("img01");
+
+ for(i=0;i< img.length;i++)
+   {    
+    img[i].onclick = function(){
+    modal.style.display = "block";
+       modalImg.src = this.src;
+ };
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+   modal.style.display = "none";
+};
