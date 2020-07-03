4abb691c7b0e0bc6d17f34e2962307bcd728b6c2
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-07-03 15:31:51 +0200

    Update modal-images.js

--- js/modal-images.js
+++ js/modal-images.js
@@ -1,4 +1,4 @@
-document.getElementById("myModal").style.display = "none";
+/*document.getElementById("myModal").style.display = "none";
 var modal = document.getElementById("myModal");
 var i;
 
@@ -20,3 +20,49 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
    modal.style.display = "none";
 };
+
+var modalBtns = document.querySelector(".modal-btns");
+
+modalBtns.addEventListener("click", function (event) {
+  var target = event.target;
+  if (target.classList.contains('prev-btn') || target.classList.contains('next-btn')) {
+    var direction = target.classList.contains('prev-btn') ? -1 : 1;
+    var currImg = document.querySelector(".modal-gallery > img[src='" + mainImg.src + "']");
+    var imgsUrls = [...allModalImgs].map(({src}) => src);
+    var nextIndex = imgsUrls.indexOf(currImg.src)  + direction;
+    nextIndex = nextIndex === imgsUrls.length ? 0 : nextIndex < 0 ? imgsUrls.length - 1 : nextIndex;
+    mainImg.src = imgsUrls[nextIndex];
+  }
+});*/
+
+document.getElementById("myModal").style.display = "none";
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = slides.length;}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
