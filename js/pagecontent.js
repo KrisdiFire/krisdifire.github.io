bb84b1972c43038cccd727e07bb84971b9c5644e
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-07-17 18:27:49 +0200

    Update pagecontent.js

--- js/pagecontent.js
+++ js/pagecontent.js
@@ -1,50 +1,49 @@
-/*funkcija za otvaranje kontenta*/
-function openContent(contentname) {
-    closeContent();
-    document.getElementById('banner-wrap').style.height = '10rem';
-    document.getElementById(contentname).classList.toggle("show-content");
-    anot();
-  }
- /*anot je specifican tekst koji se prikazuje u headeru pojedinacnih stranica*/
-  function anot(){
-
-    var o = document.getElementById('content-design');
-    var r = document.getElementById('anot-design');
-    var p = document.getElementById('content-about');
-    var t = document.getElementById('anot-about');
-    var q = document.getElementById('content-prog');
-    var y = document.getElementById('anot-web');
-
-    if (o.classList.contains('show-content')) {r.style.display = "block";}
-      else {r.style.display = "none";}
-    if (p.classList.contains('show-content')) {t.style.display = "block";}
-      else {t.style.display = "none";}
-    if (q.classList.contains('show-content')) {y.style.display = "block";}
-      else {y.style.display = "none";}
-  }
-/*Specijalna funkcija za otvaranje prednje strane (koja se inicijalno prikazuje)*/
-  function openContentFront(contentname) {
-    closeContent();
-    document.getElementById('banner-wrap').style.height = '40vh';
-    document.getElementById("willkommen").classList.remove("dont-show");
-    document.getElementById(contentname).classList.toggle("show-content");
-    anot();
-  }
-  /*funkcija koja zatvara sav kontent koji nije potreban*/
-  function closeContent() {
-    document.getElementById("front-page").classList.add("dont-show");
-    document.getElementById("willkommen").classList.add("dont-show");
-    var rl = document.querySelectorAll(".content-wrapper");
-      [].forEach.call(rl, function(closes) {
-        closes.classList.remove("show-content");});
-  }
-
 /*funkcija za parallax u headeru na front page*/
   function parallax() {
     var s = document.getElementById("willkommen");
   var yPos = 0 - window.pageYOffset/22;  
-  s.style.top = 1 + yPos + "vh";}
+  s.style.top = 10 + yPos + "rem";}
 
 window.addEventListener("scroll", function(){
     parallax(); 
 });
+
+/*Here the magic happens - animacija kontenta*/
+
+  // Detect request animation frame
+  var scroll = window.requestAnimationFrame ||
+  // IE Fallback
+  function(callback){ window.setTimeout(callback, 3000/60);};
+  var elementsToShow2 = document.querySelectorAll('.content-wrapper'); 
+  
+  function loop2() {
+  
+  Array.prototype.forEach.call(elementsToShow2, function(element){
+  if (isElementInViewport(element)) {
+  element.classList.add('show-content');
+  } else {
+  element.classList.remove('show-content');
+  }
+  });
+  
+  scroll(loop2);
+  }
+  
+  // Call the loop for the first time
+  loop2();
+  
+  // Helper function from: http://stackoverflow.com/a/7557433/274826
+  function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+  el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+  (rect.top <= 0&& rect.bottom >= 0)||
+  (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+  rect.top <= (window.innerHeight || document.documentElement.clientHeight))||
+  (rect.top >= 0 &&
+  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+  }
