4b7ec231aa336a5baaf2a2b47e73901fb8f2d59c
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-07-17 18:27:28 +0200

    Update modal-images.js

--- js/modal-images.js
+++ js/modal-images.js
@@ -1,42 +1,3 @@
-/*document.getElementById("myModal").style.display = "none";
-var modal = document.getElementById("myModal");
-var i;
-
-var img = document.getElementsByClassName("image_galery");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
- for(i=0;i< img.length;i++)
-   {    
-    img[i].onclick = function(){
-    modal.style.display = "block";
-       modalImg.src = this.src;
-       captionText.innerHTML = this.alt;
- };
-}
-
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() { 
-   modal.style.display = "none";
-};
-
-var modalBtns = document.querySelector(".modal-btns");
-
-modalBtns.addEventListener("click", function (event) {
-  var target = event.target;
-  if (target.classList.contains('prev-btn') || target.classList.contains('next-btn')) {
-    var direction = target.classList.contains('prev-btn') ? -1 : 1;
-    var currImg = document.querySelector(".modal-gallery > img[src='" + mainImg.src + "']");
-    var imgsUrls = [...allModalImgs].map(({src}) => src);
-    var nextIndex = imgsUrls.indexOf(currImg.src)  + direction;
-    nextIndex = nextIndex === imgsUrls.length ? 0 : nextIndex < 0 ? imgsUrls.length - 1 : nextIndex;
-    mainImg.src = imgsUrls[nextIndex];
-  }
-});*/
-
-
-/*zatvara modal sa slikama jer se po ucitavanju automatski otvarao*/
 document.getElementById("myModal").style.display = "none";
 
 function openModal() {
@@ -108,3 +69,4 @@ rect.top <= (window.innerHeight || document.documentElement.clientHeight))||
 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
 );
 }
+
