76e7676e81bb5e5976931568f65e4669f54a5bc1
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-07-21 18:03:23 +0200

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,9 +1,22 @@
-/*funkcija za parallax u headeru na front page*/
+/*funkcija za parallax ndf*/
 function parallax() {
+
     var s = document.getElementById("willkommen");
-  var yPos = 0 - window.pageYOffset/22;  
-  s.style.top = 10 + yPos + "rem";}
+    var o = document.getElementById("willkommen2");
+    //var b = document.getElementById("willkommen3");
+
+  var yPos = 0 - window.pageYOffset/46;  
+  s.style.top = 10 + yPos + "rem";
+
+  var yPos2 = 0 - window.pageYOffset/22;  
+  o.style.top = 25 + yPos2 + "rem";
+
+  //var yPos3 = 0 - window.pageYOffset/22;  
+  //b.style.top = 0 + yPos3 + "rem";
+
+}
   
   window.addEventListener("scroll", function(){
     parallax(); 
   });
+
