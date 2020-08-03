dd682eb9d294f3378528be616b2347168dbe7a9e
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-08-03 19:54:28 +0200

    Update modal-images.js

--- js/modal-images.js
+++ js/modal-images.js
@@ -1,37 +1,3 @@
-//Zatvara modal inace bi se otvarao na page load
-
-document.getElementById("myModal").style.display = "none";
-//otvara modal
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
-  //zatvara modal
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = slides.length;}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-  }
-  
 /*Here the magic happens - animacija slika u galeriji*/
 
   // Detect request animation frame
