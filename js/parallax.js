2aeab648245693b73926c60a6d5b99a5eeba3b14
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-08-26 02:16:09 +0200

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -4,6 +4,7 @@ function parallax() {
     var s = document.getElementById("prlx-01");
     var o = document.getElementById("prlx-02");
     var b = document.getElementById("prlx-03");
+    var e = document.getElementById("prlx-04");
     
 //brzina kojom ide element ka vrhu str (yPos 0), veci offset=sporiji skroll gore
   var yPos = 0 - window.pageYOffset/-46;  
@@ -15,6 +16,9 @@ function parallax() {
 
   var yPos3 = 0 - window.pageYOffset/-110;  
   b.style.top = -30 + yPos3 + "rem";
+
+  var yPos4 = 0 - window.pageYOffset/-110;  
+  e.style.top = -10 + yPos4 + "rem";
 
 }
   //listener koji prati skroll na stranici i po tome vrsi funkciju parralax
