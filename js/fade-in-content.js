5b37fa343b8b98256eccdd1746284e00815cc8cd
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-08-31 20:11:20 +0200

    Update fade-in-content.js

--- js/fade-in-content.js
+++ js/fade-in-content.js
@@ -2,38 +2,41 @@
 
   // Detect request animation frame
   var scroll = window.requestAnimationFrame ||
-  // IE Fallback
+  // Fallback za IE
   function(callback){ window.setTimeout(callback, 3000/60);};
+
   var elementsToShow2 = document.querySelectorAll('.content-wrapper01'); 
   
   function loop01() {
   
   Array.prototype.forEach.call(elementsToShow2, function(element){
-  if (isElementInViewport(element)) {
-  element.classList.add('show-content');
-  } else {
-  element.classList.remove('show-content');
+      if (isElementInViewport(element)) {
+        element.classList.add('show-content');
   }
-  });
+     else {
+        element.classList.remove('show-content');
+  }});
   
   scroll(loop01);
   }
   
-  // Call the loop for the first time
+  // Zovemo loop prvi put
   loop01();
   
-  // Helper function from: http://stackoverflow.com/a/7557433/274826
+  // Pomocna funkcija sa: http://stackoverflow.com/a/7557433/274826
   function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-  el = el[0];
+  
+      if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
   }
+  
   var rect = el.getBoundingClientRect();
-  return (
-  (rect.top <= 0&& rect.bottom >= 0)||
-  (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-  rect.top <= (window.innerHeight || document.documentElement.clientHeight))||
-  (rect.top >= 0 &&
-  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
+  
+      return (
+        (rect.top <= 0&& rect.bottom >= 0)||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))||
+        (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      );
   }
