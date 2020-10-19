0a74c66ee4d7898340480a75e36a258a1ac37afa
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2020-10-19 15:55:59 +0200

    Add files via upload

---
+++ bareBones-temp/js/fade-in-content.js
@@ -0,0 +1,39 @@
+/*Here the magic happens - animacija kontenta*/
+
+  // Detect request animation frame
+  var scroll = window.requestAnimationFrame ||
+  // IE Fallback
+    function(callback){ window.setTimeout(callback, 3000/60);};
+  var elementsToShow2 = document.querySelectorAll('.content-row01'); 
+  
+    function loop01() {
+  
+  Array.prototype.forEach.call(elementsToShow2, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('show-content');
+    } else {
+        element.classList.remove('show-content');
+    }
+  });
+  
+    scroll(loop01);
+  }
+  
+  // Call the loop for the first time
+    loop01();
+  
+  // Helper function from: http://stackoverflow.com/a/7557433/274826
+    function isElementInViewport(el) {
+  // special bonus for those using jQuery
+      if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+      }
+  var rect = el.getBoundingClientRect();
+      return (
+        (rect.top <= 0&& rect.bottom >= 0)||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))||
+        (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      );
+  }
