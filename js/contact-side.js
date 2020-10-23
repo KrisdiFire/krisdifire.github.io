56d3d9967ab1ea65c28e9cce0e89ba61e65d4508
Kris diFire <kristianfiredi@gmail.com>
2020-10-23 14:11:31 +0200

    fade-in/out eff

---
+++ js/contact-side.js
@@ -0,0 +1,37 @@
+var contactNav = document.getElementById("soc-menu");
+
+contactNav.classList.add("social-menu-side");
+contactNav.classList.add("soc-opacity");
+
+window.addEventListener('scroll', scrollSideM);
+
+function scrollSideM() {
+
+//kad je izmedju 
+
+  if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
+
+    contactNav.classList.add("soc-opacity-0");
+    contactNav.classList.add("social-menu");
+    
+    }
+
+//kad je gore desno
+
+   else {
+
+    contactNav.classList.add("social-menu-side");
+    contactNav.classList.remove("soc-opacity-0");
+    contactNav.classList.remove("social-menu");
+
+    }
+
+//kad je dole
+
+   if (document.body.scrollTop > 4000 || document.documentElement.scrollTop > 4000) {
+
+    contactNav.classList.remove("social-menu-side");
+    contactNav.classList.remove("soc-opacity-0");
+
+    }
+}
